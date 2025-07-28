5f52d385ebf4c35d5d93d7d008279c7bb24b7114
liamj544 <liamjackson.baby6@gmail.com>
2025-07-28 10:18:42 -0700

    Create lj ai.cpp

---
+++ lj ai.cpp
@@ -0,0 +1,75 @@
+{
+  "title": "LJ AI | LJ AI'",
+  "description": "",
+  "url": "https://studio.pickaxe.co/STUDIOKPXNX51OS1FH3UV/DZK4LMGADG",
+  "content": "LJ AI | LJ AI'\n\n===============\n\n![Image 1: LJ AI'](https://prod-pickaxe-assets.s3.us-west-2.amazonaws.com/studio_hero_photos/8859843b-5f37-4efe-8111-7e088d76a584.png)\n\nLJ AI'\n\nHome\n\n[Powered by Pickaxe](https://pickaxe.co/)\n\n5 credits left.Sign up for full access.\n\nSign up\n\n![Image 2: LJ AI's Profile Picture](https://prod-pickaxe-assets.s3.us-west-2.amazonaws.com/coverphotos/0c057224-6451-4b4e-bd5f-df3538be8e0e.png?1753565821630?1753565841659)\n\nLJ AI\n-----",
+  "metadata": {
+    "lang": "en",
+    "viewport": "width=device-width, initial-scale=1.0",
+    "robots": "index,follow",
+    "theme-color": "#141C2F",
+    "og:url": "https://studio.pickaxe.co/STUDIOKPXNX51OS1FH3UV",
+    "og:type": "website",
+    "og:image": "https://pickaxe.co/resources/images/studio-hero/pickaxe-light-hero.jpg",
+    "og:image:alt": "OG Image",
+    "og:image:type": "image/svg+xml",
+    "og:image:width": "1200",
+    "og:image:height": "630",
+    "og:site_name": "LJ AI'",
+    "twitter:title": "LJ AI'",
+    "twitter:description": "",
+    "twitter:url": "https://studio.pickaxe.co/STUDIOKPXNX51OS1FH3UV",
+    "twitter:image": "https://pickaxe.co/resources/images/studio-hero/pickaxe-light-hero.jpg",
+    "og:title": "LJ AI | LJ AI'",
+    "next-head-count": "19"
+  },
+  "external": {
+    "icon": {
+      "https://prod-pickaxe-assets.s3.us-west-2.amazonaws.com/studio_hero_photos/8859843b-5f37-4efe-8111-7e088d76a584.png": {
+        "type": "image/png"
+      }
+    },
+    "preconnect": {
+      "https://fonts.googleapis.com/": {},
+      "https://fonts.gstatic.com/": {
+        "crossorigin": ""
+      }
+    },
+    "preload": {
+      "https://studio.pickaxe.co/_next/static/media/fecba37eb4cf0fd6-s.p.ttf": {
+        "as": "font",
+        "type": "font/ttf",
+        "crossorigin": "anonymous",
+        "data-next-font": "size-adjust"
+      },
+      "https://studio.pickaxe.co/_next/static/media/4d71ab9ec562d3c7-s.p.ttf": {
+        "as": "font",
+        "type": "font/ttf",
+        "crossorigin": "anonymous",
+        "data-next-font": "size-adjust"
+      },
+      "https://studio.pickaxe.co/_next/static/media/64553737399e7e28-s.p.ttf": {
+        "as": "font",
+        "type": "font/ttf",
+        "crossorigin": "anonymous",
+        "data-next-font": "size-adjust"
+      },
+      "https://studio.pickaxe.co/_next/static/media/1a73c751efbb03b3-s.p.ttf": {
+        "as": "font",
+        "type": "font/ttf",
+        "crossorigin": "anonymous",
+        "data-next-font": "size-adjust"
+      },
+      "https://studio.pickaxe.co/_next/static/media/6871c092043d0010-s.p.ttf": {
+        "as": "font",
+        "type": "font/ttf",
+        "crossorigin": "anonymous",
+        "data-next-font": "size-adjust"
+      },
+      "https://studio.pickaxe.co/_next/static/media/96af9d79f0680225-s.p.ttf": {
+        "as": "font",
+        "type": "font/ttf",
+        "crossorigin": "anonymous",
+        "data-next-font": "size-adjust"
+      },
+      "https://studio.pickaxe.co/_next/static/media
